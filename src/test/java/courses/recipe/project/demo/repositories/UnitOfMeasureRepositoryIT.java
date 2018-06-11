8608b0dc0c840d5a378086f27bdff2585c1c7c4c
twana <twja@chess-ix.com>
2018-06-11 13:48:21 +0200

    integration test: reload spring context by using @DirtiesContext

--- src/test/java/courses/recipe/project/demo/repositories/UnitOfMeasureRepositoryIT.java
+++ src/test/java/courses/recipe/project/demo/repositories/UnitOfMeasureRepositoryIT.java
@@ -1,17 +1,17 @@
 package courses.recipe.project.demo.repositories;
 
 import courses.recipe.project.demo.model.UnitOfMeasure;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Optional;
 
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
@@ -21,9 +21,18 @@ public class UnitOfMeasureRepositoryIT {
     private UnitOfMeasureRepository classUnderTest;
 
     @Test
+    @DirtiesContext
     public void testFindByDescription() {
         Optional<UnitOfMeasure> actual = classUnderTest.findByDescription("dash");
 
         assertThat(actual.get().getDescription(), is("dash"));
+    }
+
+    @Test
+    @DirtiesContext
+    public void testFindByDescriptionCup() {
+        Optional<UnitOfMeasure> actual = classUnderTest.findByDescription("Cup");
+
+        assertThat(actual.get().getDescription(), is("Cup"));
     }
 }
