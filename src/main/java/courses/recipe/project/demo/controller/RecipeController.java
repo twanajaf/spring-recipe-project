0cf6b015ea5d08f8ef368ab66413f7f15113e4e8
twana <twja@chess-ix.com>
2018-06-11 11:56:21 +0200

    cleaning

--- src/main/java/courses/recipe/project/demo/controller/RecipeController.java
+++ src/main/java/courses/recipe/project/demo/controller/RecipeController.java
@@ -1,8 +1,6 @@
 package courses.recipe.project.demo.controller;
 
-import courses.recipe.project.demo.services.CategoryService;
 import courses.recipe.project.demo.services.RecipeService;
-import courses.recipe.project.demo.services.UnitOfMeasureService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,24 +12,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class RecipeController {
     private final RecipeService recipeService;
-    private final CategoryService categoryService;
-    private final UnitOfMeasureService unitOfMeasureService;
 
     @Autowired
-    public RecipeController(RecipeService recipeService,
-                            CategoryService categoryService,
-                            UnitOfMeasureService unitOfMeasureService) {
+    public RecipeController(RecipeService recipeService) {
         this.recipeService = recipeService;
-        this.categoryService = categoryService;
-        this.unitOfMeasureService = unitOfMeasureService;
     }
 
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(Model model) {
         model.addAttribute("recipes", recipeService.findAllRecipes());
-
-        System.out.println(categoryService.findByName("American"));
-        System.out.println(unitOfMeasureService.findByDescription("Teaspoon"));
 
         return "index";
     }
